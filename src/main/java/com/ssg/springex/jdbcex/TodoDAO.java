bf2032f7bcf7157a50fdd02fba0f1bcc5697c0bf
thatzcool <mycaptain0771@gmail.com>
2024-09-04 10:37:19 +0900

    springDay04_start

--- src/main/java/com/ssg/springex/jdbcex/TodoDAO.java
+++ src/main/java/com/ssg/springex/jdbcex/TodoDAO.java
@@ -1,2 +1,43 @@
-package com.ssg.springex.jdbcex;public class TodoDAO {
+package com.ssg.springex.jdbcex;
+
+import lombok.Cleanup;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class TodoDAO {
+
+      String now = null;
+      public String getTime(){
+
+          try(Connection connection = ConnectionUtil.INSTANCE.getConnection();
+              PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+              ResultSet resultSet = preparedStatement.executeQuery();
+          ) {
+              resultSet.next();
+
+              now = resultSet.getString(1);
+
+          }catch(Exception e){
+              e.printStackTrace();
+          }
+          return now;
+      }
+
+
+      public String getTime2() throws Exception {
+
+          @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+          @Cleanup PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+          @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+          resultSet.next();
+
+          now = resultSet.getString(1);
+          return now;
+
+      }
+
+
 }
--- src/test/java/com/ssg/dao/TodoTests.java
+++ src/test/java/com/ssg/dao/TodoTests.java
@@ -1,2 +1,23 @@
-package com.ssg.dao;public class TodoTests {
+package com.ssg.dao;
+
+import com.ssg.springex.jdbcex.TodoDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TodoTests {
+
+      private TodoDAO todoDAO;
+
+      @BeforeEach    //ready 메소드를 통해서 모든 테스트 전에 TodoDAO 타입의 객체를 생성한다.
+      public void ready(){
+          todoDAO = new TodoDAO();
+      }
+
+      @Test
+      public void testTime() throws  Exception {
+          System.out.println(todoDAO.getTime2());
+      }
+
+
+
 }
